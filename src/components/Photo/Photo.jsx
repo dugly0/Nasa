43b5f0f838e804b2d95d3558f6d415f2ea0a01cd
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-06-10 18:02:36 +0100

    Personalização da Galeria

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "./assets/css/photo.css"
 
 function Photo({ photo, onClick }) {
   // Este componente representa uma única foto na galeria
   return (
     // Container da foto, utiliza a classe 'card' do Bootstrap para um visual esteticamente agradável
-    <div className="card" onClick={onClick}>
+    <div className="card" style={{ backgroundImage: `url(${photo.img_src})`}} onClick={onClick}>
       {/* Renderiza a imagem */}
       <img
         src={photo.img_src} // URL da imagem obtida da API da NASA
         className="card-img-top" // Classe do Bootstrap para posicionar a imagem no topo do card
-        style={{ width: "200px", height: "200px" }} // Estilo inline para definir as dimensões da imagem
+        style={{ display: "none" }} // Estilo inline para definir as dimensões da imagem
         alt={`Rover ${photo.rover.name} - ${photo.camera.full_name}`} // Texto alternativo para acessibilidade, descrevendo a imagem
         onClick={onClick} // Manipulador de evento de clique, passado como prop
       />
