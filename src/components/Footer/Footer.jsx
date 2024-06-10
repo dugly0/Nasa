e6d8a1283f06e00e2b653e0a83bbcd1725494a11
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-06-11 00:01:10 +0100

    configurando reprodução do video

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ function FooterNASA() {
                             <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
                                 <iframe
                                     title="YouTube Nasa"
-                                    src="https://www.youtube.com/embed/21X5lGlDOfg"
+                                    src="https://www.youtube.com/embed/21X5lGlDOfg?autoplay=1&mute=1"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                     allowFullScreen
                                     style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
